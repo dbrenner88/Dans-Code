0ef6edcc32c3189f60083a8a29b9b5264e5255d1
Daniel <danielr.brenner@gmail.com>
2023-06-13 11:33:28 -0400

    scratch for getting up to speed in java

---
+++ learning_java/MyName.java
@@ -0,0 +1,34 @@
+public class MyName {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String name = "john";
+
+        String fName = "dan";
+        String lName = "brenner";
+        String space = " ";
+        String fullName = fName + space + lName;
+        // printing full name
+        System.out.println(fullName);
+
+        int johnNum = 22;
+        int num2;
+        num2 = 11;
+
+        // printing john name and numbers
+        System.out.println(name + ' ' + johnNum + ' ' + num2);
+
+        // printing addition
+        System.out.println(johnNum + num2);
+
+        johnNum = 154;
+
+        // printing johns number
+        System.out.println(johnNum);
+
+        final int fNum = 3;
+        System.out.println(fNum);
+
+    }
+}
