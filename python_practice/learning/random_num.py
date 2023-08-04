b1ecb23c5c8ddc4dd3a30c01efe07e26c6699bfb
Daniel <danielr.brenner@gmail.com>
2023-08-03 21:39:11 -0400

    getting back up to speed

---
+++ python_practice/learning/random_num.py
@@ -0,0 +1,17 @@
+import random as r
+
+rand_int = r.randint(0, 4)
+print(rand_int)
+
+rand_float = r.random()
+print(rand_float)
+
+rand_float2 = float(rand_int) + rand_float
+print(rand_float2)
+
+rand_flt = rand_float * 5
+print(rand_flt)
+
+states_usa = ["red", "white", "blue"]
+states_usa.append("awwww yeasshhhh")
+print(states_usa)
---
+++ python_practice/learning/scratch.py
@@ -0,0 +1,10 @@
+l_score = 45
+
+print_score = f"Your score is {l_score}"
+
+if 10 > l_score or l_score >= 90:
+    print(print_score + ", you go together like coke and mentos.")
+elif 40 <= l_score <= 50:
+    print(print_score + ", you are alright together.")
+else:
+    print(print_score + ".")
